31e4c5fb92230b53b5c22fae3ac1ea737559e435
doan2202 <doannvph39305@fpt.edu.vn>
2023-11-22 15:02:40 +0700

    up

--- admin/taikhoan/taikhoan.php
+++ admin/taikhoan/taikhoan.php
@@ -1,71 +1,55 @@
-<div class="container" id="main">
-  <div class="row justify-content-center align-items-center mt-4">
-    <div class="col-lg-6 col-lg-offset-4">
-      <form action="" method="post" enctype="multipart/form-data">
-        <legend class="text-center">Quản lý tài khoản</legend>
-        <div class="form-group row mb-3">
-          <label for="id" class="col-sm-3 col-form-label">Mã tài khoản</label>
-          <div class="col-sm-9">
-            <input type="text" class="form-control" id="id" name="id" value="auto number" readonly />
-          </div>
-        </div>
-        <div class="form-group row mb-3">
-          <label for="username" class="col-sm-3 col-form-label">Tên đăng nhập</label>
-          <div class="col-sm-9"><input type="text" class="form-control" id="username" name="username"
-              placeholder="Tên đăng nhập"></div>
-        </div>
-        <div class="form-group row mb-3">
-          <label class="col-sm-3 col-form-label" for="password">Mật khẩu</label>
-          <div class="col-sm-9"><input type="password" class="form-control" id="password" name="password"
-              placeholder="Mật khẩu"></div>
+<?php
+  $dataAll = tai_khoan_select_all();
 
-        </div>
-        <div class="form-group row mb-3">
-          <label class="col-sm-3 col-form-label" for="email">Email</label>
-          <div class="col-sm-9"><input type="email" class="form-control" id="email" name="email" placeholder="Email">
-          </div>
+  
+  
+  // $dataloai = loai_select_all_sp();
+  // // Thêm biến arrID để lưu trữ các ID của các mục đã chọn.
+  // $arrID = [];
+  
+  // // Thêm hàm deleteItems() để thực hiện việc xóa các mục đã chọn.
 
-        </div>
-        <div class="form-group row mb-3">
-          <label class="col-sm-3 col-form-label" for="phone">Số điện thoại</label>
-          <div class="col-sm-9"><input type="text" class="form-control" id="phone" name="phone"
-              placeholder="Số điện thoại"></div>
-
-        </div>
-        <div class="form-group row mb-3">
-          <label class="col-sm-3 col-form-label">Vai trò</label>
-          <div class="col-sm-9">
-            <div class="form-check form-check-inline">
-              <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="1">
-              <label for="inlineRadio1">Nhân viên</label>
-            </div>
-            <div class="form-check form-check-inline">
-              <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="2">
-              <label for="inlineRadio2">Khách hàng</label>
-            </div>
-          </div>
-        </div>
-        <div class="form-group row mb-3">
-          <label class="col-sm-3 col-form-label" for="image">Ảnh</label>
-          <div class="col-sm-9"><input type="file" class="form-control-file" id="image" name="image"></div>
-
-        </div>
-        <div class="form-group row mb-3">
-          <label class="col-sm-3 col-form-label" for="address">Địa chỉ</label>
-          <div class="col-sm-9">
-            <textarea name="address" class="form-control" id="address" cols="30" rows="5"
-              placeholder="Mô tả"></textarea>
-          </div>
-
-        </div>
-        <button type="submit" class="btn btn-primary">Cập nhật</button>
-      </form>
-      </form>
+  // function deleteItems()
+  // {
+  //   // Lấy tất cả các ID của các mục đã chọn.
+  //   foreach ($_POST['check'] as $id) {
+  //     $arrID[] = $id;
+  //   }
+  
+  //   // Xóa các mục đã chọn khỏi cơ sở dữ liệu.
+  //   hang_hoa_delete_multi($arrID);
+  
+  //   header('Location: index.php?act=sanpham');
+    
+  // }
+  
+  // // Sửa đổi hàm loai_delete() để chấp nhận một mảng các ID làm tham số.
+  // function hang_hoa_delete_multi($arrID)
+  // {
+  //   // Xóa từng mục khỏi cơ sở dữ liệu.
+  //   foreach ($arrID as $id) {
+  //     san_pham_delete($id);
+  //   }
+  // }
+  
+  // // Thêm một sự kiện click cho nút `XÓA CÁC MỤC ĐÃ CHỌN` để gọi hàm `deleteItems()`.
+  // if (isset($_POST['btnDelete'])) {
+  //   deleteItems();
+  // }
+?>
+<form action="" method="post">
+  <legend class="text-center mb-4">Danh sách tài khoản</legend>
+  <div class="container" style="max-width: 90%">
+    <div class="row mb10 mt-2">
+      <a href="#" class="col-auto"><input class="btn btn-primary mr10 checked" type="button" value="CHỌN TẤT CẢ"></a>
+      <a href="#" class="col-auto"><input class="btn btn-primary mr10 unchecked" type="button"
+          value="BỎ CHỌN TẤT CẢ"></a>
+      <a href="#" class="col-auto"><input class="btn btn-primary mr10" type="submit" name="btnDelete"
+          value="XÓA CÁC MỤC ĐÃ CHỌN"></a>
+      <a href="index.php?act=edit&page=taikhoan" class="col-auto"> <input class="btn btn-primary mr20" type="button"
+          value="NHẬP THÊM"></a>
     </div>
-  </div>
-</div>
-<div class="container" style="max-width: 90%">
-  <table class="table table-striped">
+    <table class="table table-striped">
     <thead>
       <tr>
         <th scope="col">Mã</th>
@@ -79,36 +63,28 @@
       </tr>
     </thead>
     <tbody>
+    <?php foreach ($dataAll as $key => $value) : ?>
       <tr>
-        <th scope="row">1</th>
-        <td>ab</td>
-        <td>1</td>
-        <td>45535</td>
-        <td>$435</td>
-        <td>gdgffb</td>
-        <td>gdgffb</td>
+        <th scope="row"><?= $value['id']; ?></th>
+        <td><?= $value['username']; ?></td>
+        <td><?= $value['password']; ?></td>
+        <td><?= $value['email']; ?></td>
+        <td><?= $value['sdt']; ?></td>
+        <td><?= $value['dia_tri']; ?></td>
+        <td><?= $value['vai_tro']; ?></td>
+        
         <td>
-          <a class="btn btn-danger">
-            Xóa
-          </a>
-        </td>
+        <a href="index.php?DTK=<?= $value['id']; ?>&page=taikhoan&act=xoa" class="btn btn-danger"
+          onclick="return confirm('Bạn có muốn xóa hay không?')">
+          Xóa
+        </a>
+            <a href="index.php?idTK=<?= $value['id']; ?>&page=taikhoan&act=edit" class="btn btn-warning"> Sửa </a>
+          </td>
       </tr>
-      <tr>
-        <th scope="row">2</th>
-        <td>ab</td>
-        <td>1</td>
-        <td>45535</td>
-        <td>$435</td>
-        <td>$435</td>
-        <td>gdgffb</td>
-        <td>
-          <a class="btn btn-danger">
-            Xóa
-          </a>
-        </td>
-      </tr>
+      <?php endforeach ?>
     </tbody>
   </table>
-</div>
+  </div>
+</form>
 </div>
 </div>
